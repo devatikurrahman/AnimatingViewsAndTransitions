fc9dde51228283a5fed7fd6f83d9e24e069b1e5e
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-24 00:08:20 +0600

    Initial commit

---
+++ AnimatingViewsAndTransitions/AnimatingViewsAndTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingViewsAndTransitionsApp.swift
+//  AnimatingViewsAndTransitions
+//
+//  Created by Atikur Rahman on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingViewsAndTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
